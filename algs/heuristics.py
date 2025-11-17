dd0ff3a1ba5d6c8b110425eb5045cff6e37e3dfb
John Robert Ierfino <jr@MacBook-Pro-2.local>
2025-11-17 16:09:27 -0500

    Remove old project files

--- algs/heuristics.py
+++
@@ -1,4 +0,0 @@
-from algs.firstfit import firstfit
-def firstfit_luf(G):
-    vertices = sorted(G.vertices(), key=lambda v: len(G.neighbors(v)), reverse=True)
-    return firstfit(G, order=vertices)
